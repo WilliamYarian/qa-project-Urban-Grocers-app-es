aeb84967c3e3091b2d5cafe018dd396d08948e89
William Alexander Yarian <williamalexanderyarian@gmail.com>
2025-03-20 11:43:52 -0600

    Se corrige la prueba cuatro de 'positive assert' a 'negative assert' para que sean 5 pruebas aprobadas y 5 no aprobadas

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-13b0ac83-0285-41e2-b6f5-bf6cff1b1cef.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-67e607f7-b90c-4bc8-b236-66f67b6a1590.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
 
